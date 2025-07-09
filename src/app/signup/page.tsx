5b227275f891afc942f7dd46738eee65746dad13
Yash koolwal <yashkoolwal27@gmail.com>
2025-07-09 10:37:09 +0000

    in dob while click on year i can see the year chart

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState } from 'react';
@@ -179,6 +180,9 @@ export default function SignupPage() {
                       <PopoverContent className="w-auto p-0" align="start">
                         <Calendar
                           mode="single"
+                          captionLayout="dropdown-buttons"
+                          fromYear={1900}
+                          toYear={new Date().getFullYear()}
                           selected={field.value}
                           onSelect={field.onChange}
                           disabled={(date) =>
