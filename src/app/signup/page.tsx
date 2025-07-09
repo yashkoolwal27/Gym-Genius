221791f480aaf0cbab4de89c5ac29145ae84a474
Yash koolwal <yashkoolwal27@gmail.com>
2025-07-09 10:47:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CaptionDropdowns

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -179,7 +179,6 @@ export default function SignupPage() {
                       <PopoverContent className="w-auto p-0" align="start">
                         <Calendar
                           mode="single"
-                          captionLayout="dropdown-buttons"
                           fromYear={1900}
                           toYear={new Date().getFullYear()}
                           selected={field.value}
