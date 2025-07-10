f889284c6bd01bfc461f33df8d4ec53a18e5fa49
Yash koolwal <yashkoolwal27@gmail.com>
2025-07-10 03:45:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DietLogger>
    
    with

--- src/lib/foods.ts
+++ src/lib/foods.ts
@@ -7,8 +7,6 @@ export const foodData = {
         { name: "Banana", image: "https://placehold.co/200x200.png", hint: "yellow banana", calories: 105, protein: 1.3 },
         { name: "Strawberry", image: "https://placehold.co/200x200.png", hint: "red strawberry", calories: 32, protein: 0.7 },
         { name: "Blueberry", image: "https://placehold.co/200x200.png", hint: "fresh blueberry", calories: 57, protein: 0.7 },
-        { name: "Tomato", image: "https://placehold.co/200x200.png", hint: "red tomato", calories: 18, protein: 0.9 },
-        { name: "Avocado", image: "https://placehold.co/200x200.png", hint: "green avocado", calories: 160, protein: 2.0 },
         { name: "Orange", image: "https://placehold.co/200x200.png", hint: "fresh orange", calories: 47, protein: 0.9 },
         { name: "Grapes", image: "https://placehold.co/200x200.png", hint: "purple grapes", calories: 69, protein: 0.7 },
         { name: "Watermelon", image: "https://placehold.co/200x200.png", hint: "watermelon slice", calories: 30, protein: 0.6 },
